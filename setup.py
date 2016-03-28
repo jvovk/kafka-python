a91dbfb38ad015eabc8b81a32b9c1dd18ac88816
Yuliia Vovk <smyulia96@gmail.com>
2016-03-28 12:29:48 +0300

    [second commit]

--- setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-from distutils.core import setup
-
-setup(name='Kafka-python',
-      version='1.0',
-      description='Simple kafka python producer/consumer saving to mongo',
-      packages=['pymongo', 'kafka-python'],
-     )
